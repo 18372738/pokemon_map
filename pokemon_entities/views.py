0a3610dd20a87d525248d3c102c06278c60ae2d1
18372738 <133884450+18372738@users.noreply.github.com>
2024-03-07 00:20:38 +0300

    add specs to PokemonEntity model.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,7 +3,7 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from .models import Pokemon
+from .models import Pokemon, PokemonEntity
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -32,17 +32,19 @@ def show_all_pokemons(request):
         pokemons = json.load(database)['pokemons']
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
-            )
+    for pokemon_entity in PokemonEntity.objects.all():
+        if pokemon_entity.pokemon.photo:
+            photo_url = request.build_absolute_uri(pokemon_entity.pokemon.photo.url)
+        else:
+            photo_url = None
+        add_pokemon(
+            folium_map, pokemon_entity.lat,
+            pokemon_entity.lon,
+            photo_url,
+        )
 
     pokemons_on_page = []
-    pokemons = Pokemon.objects.all()
-    for pokemon in pokemons:
+    for pokemon in Pokemon.objects.all():
         if pokemon.photo:
             photo_url = request.build_absolute_uri(pokemon.photo.url)
         else:
